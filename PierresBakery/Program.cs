0d3ebcb385e6a71931e298ce5094aa67e85cd1d5
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-25 11:47:22 -0700

    adjusted UI elements to give a delayed response after user provides an input

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -13,6 +13,7 @@ class Program
     string buyBreadResponse = Console.ReadLine().ToLower();
     if (buyBreadResponse.Substring(0,1) == "y")
     {
+      System.Threading.Thread.Sleep(800);
       Console.WriteLine("---------------------------------------------------");
       Console.WriteLine("Very good! How many loaves of bread would you like?");
       int breadQuantityResponse = int.Parse(Console.ReadLine());
@@ -20,31 +21,39 @@ class Program
       Console.WriteLine("Excellent!");
       Bread newBread = new Bread();
       breadTotal = newBread.BreadCost(breadQuantityResponse);
+      System.Threading.Thread.Sleep(600);
     }
+    System.Threading.Thread.Sleep(600);
     Console.WriteLine("Would you like to buy any Pastries today? They are $2 each or 3 for $5.");
     string buyPastryResponse = Console.ReadLine().ToLower();
     if (buyPastryResponse.Substring(0,1) == "y")
     {
+      System.Threading.Thread.Sleep(800);
       Console.WriteLine("--------------------------------------------");
       Console.WriteLine("Very good! How many pastries would you like?");
       int pastryQuantityResponse = int.Parse(Console.ReadLine());
       Console.WriteLine("Excellent!");
       Pastry newPastry = new Pastry();
       pastryTotal = newPastry.PastryCost(pastryQuantityResponse);
+      System.Threading.Thread.Sleep(800);
     }
     int purchaseTotal = breadTotal + pastryTotal;
     if (purchaseTotal == 0)
     {
+      System.Threading.Thread.Sleep(800);
       Console.WriteLine("------------------------------------------------------------------------------");
       Console.WriteLine("We are sorry to see you go without any delicious baked goods! Have a nice day!");
     }
     else
     {
-      Console.WriteLine("----------------------");
+      System.Threading.Thread.Sleep(800);
+      Console.WriteLine("-----------------------");
       Console.WriteLine("You total today is " + "$" + purchaseTotal + ".");
-      Console.WriteLine("---------------------------");
-      Console.WriteLine("(Money is being exchanged)");
-      Console.WriteLine("---------------------------");
+      System.Threading.Thread.Sleep(1700);
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("(Money is being exchanged...)");
+      Console.WriteLine("-----------------------------");
+      System.Threading.Thread.Sleep(2600);
       Console.WriteLine("We really appreciate you coming in today!");
     }
   }
