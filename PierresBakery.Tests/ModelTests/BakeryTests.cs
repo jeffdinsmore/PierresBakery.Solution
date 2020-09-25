d22efb0c048cdc541be8af73c686355c1b3ca281
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-25 11:27:09 -0700

    tested price of buy 3 for 5 pastries[A

--- PierresBakery.Tests/ModelTests/BakeryTests.cs
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -11,7 +11,6 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void FindTotalCostOfMultipleBreads_InputBreadQuantityAndGetPrice_ReturnPriceOfMultipleBreads()
     {
-      // testing code will go here
       Bread testBread = new Bread();
       int quantity = 7;
       Assert.AreEqual(25, testBread.BreadCost(quantity));
@@ -20,7 +19,6 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void FindTotalCostOfBread_InputBreadQuantityAndGetPrice_ReturnPriceOfBuy2Get1Free()
     {
-      // testing code will go here
       Bread testBread = new Bread();
       int quantity = 3;
       Assert.AreEqual(10, testBread.BreadCost(quantity));
@@ -29,7 +27,6 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void FindTotalCostOfOneLoaf_InputBreadQuantityAndGetPrice_ReturnPriceOfLoaf()
     {
-      // testing code will go here
       Bread testBread = new Bread();
       int quantity = 1;
       Assert.AreEqual(5, testBread.BreadCost(quantity));
@@ -39,18 +36,23 @@ namespace PierresBakery.Tests
   public class PastryTests
   {
     [TestMethod]
-    public void FindTotalCostOfPastry_InputPastryQuantityAndGetPrice_ReturnPriceOfTwoOrBuy3For5()
+    public void FindTotalCostOfPastries_InputPastryQuantityAndGetPrice_ReturnPriceOfMultiplePastries()
     {
-      // testing code will go here
       Pastry testPastry = new Pastry();
       int quantity = 7;
       Assert.AreEqual(12, testPastry.PastryCost(quantity));
     }
 
     [TestMethod]
+      public void FindTotalCostOf3Pastries_InputPastryQuantityAndGetPrice_ReturnPriceOfBuy3For5()
+    {
+      Pastry testPastry = new Pastry();
+      int quantity = 3;
+      Assert.AreEqual(6, testPastry.PastryCost(quantity));
+    }
+    [TestMethod]
     public void FindCostOfOnePastry_InputPastryQuantityAndGetPrice_ReturnPriceOfOnePastry()
     {
-      // testing code will go here
       Pastry testPastry = new Pastry();
       int quantity = 1;
       Assert.AreEqual(2, testPastry.PastryCost(quantity));
