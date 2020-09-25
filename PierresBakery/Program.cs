d2c4a44366809d99528f07219a9142b8dbd1612f
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-25 10:55:50 -0700

    successfully tested mvp for UI of pierres bakery

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,40 +1,50 @@
 using System;
-using System.Boolean;
 using PierresBakery.Models;
 
 class Program
 {
   static void Main()
   {
-    int breadTotal, pastryTotal = 0;
+    int breadTotal = 0;
+    int pastryTotal = 0;
     Console.WriteLine("Welcome to Pierre's Bakery! The best baked goods in town!");
     Console.WriteLine("We sell bread and we sell pastries.");
     Console.WriteLine("Would you like to buy any bread today? They are $5 each or buy 2 get one free.");
     string buyBreadResponse = Console.ReadLine().ToLower();
-    if (buyBreadResponse == "y" || buyBreadResponse == "yes" || buyBreadResponse == "ya" || buyBreadResponse == "yep" || buyBreadResponse == "yeah")
+    if (buyBreadResponse.Substring(0,1) == "y")
     {
+      Console.WriteLine("---------------------------------------------------");
       Console.WriteLine("Very good! How many loaves of bread would you like?");
       int breadQuantityResponse = int.Parse(Console.ReadLine());
+      Console.WriteLine("----------");
       Console.WriteLine("Excellent!");
-      breadTotal = Bread.BreadCost(breadQuantityResponse);
+      Bread newBread = new Bread();
+      breadTotal = newBread.BreadCost(breadQuantityResponse);
     }
     Console.WriteLine("Would you like to buy any Pastries today? They are $2 each or 3 for $5.");
     string buyPastryResponse = Console.ReadLine().ToLower();
-    if (buyPastryResponse == "y" || buyPastryResponse == "yes" || buyPastryResponse == "ya" || buyPastryResponse == "yep" || buyPastryResponse == "yeah")
+    if (buyPastryResponse.Substring(0,1) == "y")
     {
+      Console.WriteLine("--------------------------------------------");
       Console.WriteLine("Very good! How many pastries would you like?");
       int pastryQuantityResponse = int.Parse(Console.ReadLine());
       Console.WriteLine("Excellent!");
-      pastryTotal = Pastry.PastryCost(pastryQuantityResponse);
+      Pastry newPastry = new Pastry();
+      pastryTotal = newPastry.PastryCost(pastryQuantityResponse);
     }
-    if ((breatTotal + pastryTotal) == 0)
+    int purchaseTotal = breadTotal + pastryTotal;
+    if (purchaseTotal == 0)
     {
+      Console.WriteLine("------------------------------------------------------------------------------");
       Console.WriteLine("We are sorry to see you go without any delicious baked goods! Have a nice day!");
     }
     else
     {
-      Console.WriteLine("You total today is " + "$" + breadTotal + pastryTotal + ".");
-      Console.WriteLine("(Money is being exchanged");
+      Console.WriteLine("----------------------");
+      Console.WriteLine("You total today is " + "$" + purchaseTotal + ".");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("(Money is being exchanged)");
+      Console.WriteLine("---------------------------");
       Console.WriteLine("We really appreciate you coming in today!");
     }
   }
