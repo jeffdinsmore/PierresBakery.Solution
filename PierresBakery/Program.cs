f4eaa7d868a1a324772b5e5a1cadaa92e2c7295a
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-28 23:26:10 -0700

    updated UI to handle two different types of bread

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,27 +7,44 @@ class Program
   {
     int breadTotal = 0;
     int pastryTotal = 0;
+    int twentyOneGrainQuantity = 0;
+    int powerseedQuantity = 0;
     Console.WriteLine("Welcome to Pierre's Bakery! The best baked goods in town!");
     Console.WriteLine("We sell bread and we sell pastries.");
-    Console.WriteLine("Would you like to buy any bread today? Twenty One Grain is $5 each or buy 2, get 1 free and Powerseed is $6 each or buy 3, get one free.");
+    Console.WriteLine("Would you like to buy any bread today? Twenty One Grain is $5 each or buy 2, get 1 free and Powerseed is $6 each or buy 3, get 1 free.");
     string buyBreadResponse = Console.ReadLine().ToLower();
     if (buyBreadResponse.Substring(0,1) == "y")
     {
       System.Threading.Thread.Sleep(800);
-      Console.WriteLine("---------------------------------------------------");
+      Console.WriteLine("-------------------------------------------------------------------------------------");
       Console.WriteLine("Very good! What type of bread would you like? Twenty One Grain or Powerseed or both?");
       string breadType = Console.ReadLine().ToLower();
-      if (breadType.Substring(0,1) == "t")
-      {
-        Console.WriteLine("How many loaves would you like?");
-
-      }
       
-      int breadQuantityResponse = int.Parse(Console.ReadLine());
+      if (breadType.Substring(0,1) == "b")
+      {
+        System.Threading.Thread.Sleep(600);
+        Console.WriteLine("How many loaves of 21 Grain bread would you like?");
+        twentyOneGrainQuantity = int.Parse(Console.ReadLine());
+        System.Threading.Thread.Sleep(600);
+        Console.WriteLine("How many loaves of Powerseed bread would you like?");
+        powerseedQuantity = int.Parse(Console.ReadLine());
+      }
+      else if (breadType.Substring(0,1) == "p")
+      {
+        System.Threading.Thread.Sleep(600);
+        Console.WriteLine("How many loaves of Powerseed bread would you like?");
+        powerseedQuantity = int.Parse(Console.ReadLine());
+      }
+      else if (breadType.Substring(0,1) == "t")
+      {
+        System.Threading.Thread.Sleep(600);
+        Console.WriteLine("How many loaves of 21 Grain bread would you like?");
+        twentyOneGrainQuantity = int.Parse(Console.ReadLine());
+      }
       Console.WriteLine("----------");
       Console.WriteLine("Excellent!");
       Bread newBread = new Bread();
-      breadTotal = newBread.BreadCost(breadQuantityResponse, 0);
+      breadTotal = newBread.BreadCost(twentyOneGrainQuantity, powerseedQuantity);
       System.Threading.Thread.Sleep(600);
     }
     System.Threading.Thread.Sleep(600);
@@ -60,7 +77,7 @@ class Program
       Console.WriteLine("-----------------------------");
       Console.WriteLine("(Money is being exchanged...)");
       Console.WriteLine("-----------------------------");
-      System.Threading.Thread.Sleep(2600);
+      System.Threading.Thread.Sleep(2000);
       Console.WriteLine("We really appreciate you coming in today!");
     }
   }
