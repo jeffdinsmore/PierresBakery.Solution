8c837a980fc03db9778e58c55ab318b1ea15f818
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-29 23:20:54 -0700

    adjusted the code to the color for the console after testing to make everything white except the questions being asked to the user

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -15,38 +15,46 @@ class Program
     Console.WriteLine("We sell bread and we sell pastries.");
     Console.ForegroundColor = ConsoleColor.Cyan; 
     Console.WriteLine("Would you like to buy any bread today? Twenty One Grain is $5 each or buy 2, get 1 free and Powerseed is $6 each or buy 3, get 1 free.");
+    Console.ForegroundColor = ConsoleColor.White;
     string buyBreadResponse = Console.ReadLine().ToLower();
     if (buyBreadResponse.Substring(0,1) == "y")
     {
       System.Threading.Thread.Sleep(800);
-      Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("-------------------------------------------------------------------------------------");
       Console.ForegroundColor = ConsoleColor.Cyan;
       Console.WriteLine("Very good! What type of bread would you like? Twenty One Grain or Powerseed or both?");
+      Console.ForegroundColor = ConsoleColor.White;
       string breadType = Console.ReadLine().ToLower();
       
       if (breadType.Substring(0,1) == "b")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many loaves of 21 Grain bread would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         twentyOneGrainQuantity = int.Parse(Console.ReadLine());
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many loaves of Powerseed bread would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         powerseedQuantity = int.Parse(Console.ReadLine());
       }
       else if (breadType.Substring(0,1) == "p")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many loaves of Powerseed bread would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         powerseedQuantity = int.Parse(Console.ReadLine());
       }
       else if (breadType.Substring(0,1) == "t")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many loaves of 21 Grain bread would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         twentyOneGrainQuantity = int.Parse(Console.ReadLine());
       }
-      Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("----------");
       Console.WriteLine("Excellent!");
       Bread newBread = new Bread();
@@ -56,37 +64,45 @@ class Program
     System.Threading.Thread.Sleep(600);
     Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("Would you like to buy any Pastries today? Doughnuts are $1 each or 3 for $2 and muffins are $2 each or 3 for $5.");
+    Console.ForegroundColor = ConsoleColor.White;
     string buyPastryResponse = Console.ReadLine().ToLower();
     if (buyPastryResponse.Substring(0,1) == "y")
     {
       System.Threading.Thread.Sleep(800);
-      Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("-----------------------------------------------------");
       Console.ForegroundColor = ConsoleColor.Cyan;
       Console.WriteLine("Very good! Would you like doughnuts, muffins or both?");
+      Console.ForegroundColor = ConsoleColor.White;
       string pastryTypeResponse = Console.ReadLine().ToLower();
       if (pastryTypeResponse.Substring(0,1) == "b")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many doughnuts would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         doughnutQuantity = int.Parse(Console.ReadLine());
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many muffins would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         muffinQuantity = int.Parse(Console.ReadLine());
       }
       else if (pastryTypeResponse.Substring(0,1) == "d")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many doughnuts would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         doughnutQuantity = int.Parse(Console.ReadLine());
       }
       else if (pastryTypeResponse.Substring(0,1) == "m")
       {
         System.Threading.Thread.Sleep(600);
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("How many muffins would you like?");
+        Console.ForegroundColor = ConsoleColor.White;
         muffinQuantity = int.Parse(Console.ReadLine());
       }
-      Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("----------");
       Console.WriteLine("Excellent!");
       Pastry newPastry = new Pastry();
@@ -97,7 +113,6 @@ class Program
     if (purchaseTotal == 0)
     {
       System.Threading.Thread.Sleep(800);
-      Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("------------------------------------------------------------------------------");
       Console.WriteLine("We are sorry to see you go without any delicious baked goods! Have a nice day!");
     }
@@ -113,6 +128,5 @@ class Program
       System.Threading.Thread.Sleep(2000);
       Console.WriteLine("We really appreciate you coming in today!");
     }
-    Console.ForegroundColor = ConsoleColor.White;
   }
 }
